852a43d5a339fccd5c1f16b63a066169a110086b
IOksa <117656767+IOksa@users.noreply.github.com>
2023-04-22 21:02:51 +0300

    update

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,4 @@
-import './css/styles.css';
+import '../css/styles.css';
 import API from './fetchCountries';
 import debounce from 'lodash.debounce';
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
@@ -16,12 +16,11 @@ refs.input.addEventListener('input', debounce(onInputCountry, DEBOUNCE_DELAY));
 
 function onInputCountry(){
     const searchCountry=refs.input.value.trim();
-    console.log('searchCountry=', searchCountry);
+    
     if(searchCountry!==''){
         API.fetchCountries(searchCountry)
         .then(renderCountriesInfo)
         .catch(onFetchError);
-        // .finally(refs.input.value='');
     }
     else{
         clearMarkup();
@@ -32,8 +31,6 @@ function onInputCountry(){
 }
 
 function renderCountriesInfo(arrayCountries){
-    console.log(arrayCountries);
-  
     if(arrayCountries.length>10){
         clearMarkup();
         Notify.info("Too many matches found. Please enter a more specific name.");
@@ -61,15 +58,11 @@ function createMarkupListCountries(arr) {
        )
        .join("");
        
-       console.log(markup);
-
        refs.ul.innerHTML=markup;
   }
  
 function createMarkupCountryCard(arr){
-    console.log(arr);
     const [array]=arr;
-    console.log('array=', array)
     const {
         flags: {svg, alt},
         name:{common},
@@ -79,13 +72,10 @@ function createMarkupCountryCard(arr){
 
     }=array;
     const capitalCountry=capital[0];
-    console.log('languages=', languages);
-
     const langValues=Object.values(languages).join(', ');
-    console.log('langValues=', langValues);
-
+    
     const markup=`<div class="country-thumb"><img src="${svg}" alt="${alt}" width="50px" height="30px"/><p class="country-name">${common}</p></div><p class="country-description">Capital: <span class="country-value">${capitalCountry}</span></p><p class="country-description">Population: <span class="country-value">${population}</span></p><p class="country-description">Languages: <span class="country-value">${langValues}</span></p>`;  
-    console.log(markup);
+    
     refs.div.innerHTML=markup;    
 }
 
